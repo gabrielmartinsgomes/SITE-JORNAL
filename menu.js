7454123b55283e53fcf5a17adc68296e4bf6e912
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2023-07-30 22:23:33 -0300

    atualizacao

--- menu.js
+++ menu.js
@@ -14,31 +14,3 @@ function toggleBusca() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('ativo');
   }
-
-btnBusca.addEventListener('click', toggleBusca)
-
-const changeThemeBtn = document.querySelector("#change-theme");
-
-function toggleDarkMode() {
-  document.body.classList.toggle("dark");
-}
-
-function loadTheme() {
-  const darkMode = localStorage.getItem("dark");
-
-  if (darkMode) {
-    toggleDarkMode();
-  }
-}
-
-loadTheme();
-
-changeThemeBtn.addEventListener("change", function () {
-  toggleDarkMode();
-
-  localStorage.removeItem("dark");
-
-  if (document.body.classList.contains("dark")) {
-    localStorage.setItem("dark", 1);
-  }
-});
